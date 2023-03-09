2835baf600bd4425bad073e36d05a9c0d71b42a8
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-03-08 21:29:05 -0500

    changed footer text color to black

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,10 @@ function MyApp({Component, pageProps}) {
             <BetterHeader/>
             <Component {...pageProps} />
             <div className="mx-auto pt-4 pb-4" id="footer">
-                <div className="text-sm text-white text-center md:text-md">
+                <div className="text-sm text-lightBlack text-center md:text-md">
                     This project is sponsored by the Center for Digital Humanities at Princeton University
                 </div>
-                <div className="text-sm text-white text-center mt-2 md:text-md">
+                <div className="text-sm text-lightBlack text-center mt-2 md:text-md">
                     Copyright © 2022
                 </div>
             </div>
